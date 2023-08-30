8849ca1a2143d5a5f4bd4942b4abea0f19893680
bijx <bsamiee@hotmail.com>
2023-08-29 21:26:19 -0400

    added constants module

---
+++ components/constants.js
@@ -0,0 +1,3 @@
+export const VIDEO_ID_LENGTH = 11;
+export const YOUTUBE_URL_REGEX =
+  /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
