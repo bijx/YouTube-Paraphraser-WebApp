831d18445f956556777515c1ced46de4afd296f6
bijx <bsamiee@hotmail.com>
2023-08-29 21:26:26 -0400

    added elements module

---
+++ components/elements.js
@@ -0,0 +1,13 @@
+export const urlField = document.querySelector('.url-field');
+export const actionsElement = document.getElementById('actions');
+export const logoElement = document.getElementById('logo');
+export const loadElement = document.getElementById('load');
+export const containerElement = document.getElementById('container');
+export const summaryElement = document.getElementById('summary');
+export const thumbnailElement = document.getElementById('thumbnail');
+export const thumbUrlElement = document.getElementById('thumb-url');
+export const titleElement = document.getElementById('title');
+export const authorElement = document.getElementById('author');
+export const authorUrlElement = document.getElementById('author-url');
+export const paraphrasedTextElement =
+  document.getElementById('paraphrased-text');
