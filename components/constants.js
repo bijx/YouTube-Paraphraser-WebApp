5e9fe5d492a3257596edf9036aedcffdf67fe030
bijx <bsamiee@hotmail.com>
2023-12-03 00:28:05 -0500

    updated api

--- components/constants.js
+++ components/constants.js
@@ -7,5 +7,5 @@ export const YOUTUBE_URL_REGEX =
   /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
 export const API_ENDPOINT = isDevelopment
   ? 'http://localhost:8081/youtube/'
-  : 'https://yt-paraphraser-v1-2xko3wx73q-uc.a.run.app/youtube/';
+  : 'https://yt-paraphraser-v1-5-1-wf45exjwaq-uc.a.run.app/youtube/';
 export const URL_HOME = isDevelopment ? '' : '/YouTube-Paraphraser-WebApp/';
